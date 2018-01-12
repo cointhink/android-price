4ff4cac54a366a1e00562b48d83af92ffa80f56c
Don Park <donp@donp.org>
2018-01-12 12:04:27 +0700

    mangle magic

--- src/com/cointhink/cmc/CoinAdapter.java
+++ src/com/cointhink/cmc/CoinAdapter.java
@@ -3,6 +3,7 @@ package com.cointhink.cmc;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,24 +31,66 @@ public class CoinAdapter extends ArrayAdapter<Coin> {
         ((TextView) convertView.findViewById(R.id.coinSymbol))
                 .setText(coin.symbol);
         ((TextView) convertView.findViewById(R.id.coinPrice))
-                .setText("$"+coin.price);
+                .setText("$" + priceMangle(coin.price));
 
         // Return the completed view to render on screen
         return convertView;
     }
 
+    private String priceMangle(String price) {
+        int decimalPos = price.indexOf(".");
+        int sigFigInt, sigFigDec;
+        String intPart, decPart;
+        if (decimalPos == -1) {
+            intPart = price;
+            decPart = "";
+        } else {
+            intPart = price.substring(0, decimalPos);
+            decPart = price.substring(decimalPos + 1, price.length());
+        }
+        String mangled = intPart;
+        int decSize = Math.max(0, 5 - intPart.length());
+        String decMangled = decMassage(decPart, decSize);
+        if (decMangled.length() > 0) {
+            mangled = mangled + "." + decMangled;
+        }
+        Log.d("CMC", "pricemangle " + price + " intPart:" + intPart
+                + " decPart(" + decSize + "):" + decPart + " => " + mangled);
+        return mangled;
+    }
+
+    private String decMassage(String decPart, int bestLen) {
+        if (bestLen == 1) {
+            bestLen = 2; //USD-like
+        }
+        if (bestLen == 0) {
+            decPart = "";
+        }
+        if (decPart.length() > 0) {
+            int padding = bestLen - decPart.length();
+
+            for (int i = 0; i < padding; i++) {
+                decPart = decPart + "0";
+            }
+        }
+        if (decPart.length() > bestLen) {
+            decPart = decPart.substring(0, bestLen);
+        }
+        return decPart;
+    }
+
     public String longParse(String longStr) {
         int decimalPos = longStr.indexOf(".");
         if (decimalPos > 0) {
-         longStr = longStr.substring(0, decimalPos);
+            longStr = longStr.substring(0, decimalPos);
         }
         long longCap = Long.parseLong(longStr);
         String capStr = "?M";
         if (longCap > 1e6) {
-            capStr = (longCap/1000000L)+"M";
+            capStr = (longCap / 1000000L) + "M";
         }
         if (longCap > 1e9) {
-            capStr = (longCap/1000000000L)+"B";
+            capStr = (longCap / 1000000000L) + "B";
         }
         return capStr;
     }
