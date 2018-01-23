a17deabdf01c1c8b490b1f3ad0c9de0f60290af7
Don Park <donp@donp.org>
2018-01-23 16:35:17 +0700

    there is no end to the Android Lore

--- src/com/cointhink/cmc/Constants.java
+++ src/com/cointhink/cmc/Constants.java
@@ -2,4 +2,5 @@ package com.cointhink.cmc;
 
 public interface Constants {
     static final String APP_TAG = "CMC";
+    static final String PREFERENCE_DATA_SOURCE = "data_source";
 }
--- src/com/cointhink/cmc/PrefsFragment.java
+++ src/com/cointhink/cmc/PrefsFragment.java
@@ -1,10 +1,18 @@
 package com.cointhink.cmc;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v4.preference.PreferenceFragment;
 import android.util.Log;
 
-public class PrefsFragment extends PreferenceFragment {
+public class PrefsFragment extends PreferenceFragment
+        implements OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPrefs;
+    private final String[] keys = { Constants.PREFERENCE_DATA_SOURCE };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,22 @@ public class PrefsFragment extends PreferenceFragment {
         Log.d(Constants.APP_TAG, "prefFrag onCreate");
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getActivity());
+        sharedPrefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+            String key) {
+        Log.d(Constants.APP_TAG, "onSharedPreferenceChanged " + key);
+        Preference preference = getPreferenceScreen().findPreference(key);
+        if (preference != null) {
+        }
     }
 }
