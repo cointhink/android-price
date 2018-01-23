61952f6df29b4cc9eab2159ad95cb63e4bb1012a
Don Park <donp@donp.org>
2018-01-23 15:53:24 +0700

    prefs progress

--- src/com/cointhink/cmc/PrefsFragment.java
+++ src/com/cointhink/cmc/PrefsFragment.java
@@ -2,14 +2,15 @@ package com.cointhink.cmc;
 
 import android.os.Bundle;
 import android.support.v4.preference.PreferenceFragment;
+import android.util.Log;
 
 public class PrefsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.d(Constants.APP_TAG, "prefFrag onCreate");
         // Load the preferences from an XML resource
-        //addPreferencesFromResource(R.xml.preferences);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
