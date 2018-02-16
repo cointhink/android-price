342fd7a2a4547de840e05de30040011a4725f281
Don Park <donp@donp.org>
2018-02-16 14:05:10 +0700

    0.90

--- src/com/cointhink/cmc/Constants.java
+++ src/com/cointhink/cmc/Constants.java
@@ -1,9 +1,9 @@
 package com.cointhink.cmc;
 
 public interface Constants {
-    static final String VERSION = "0.89";
+    static final String VERSION = "0.90";
 
-    static final String APP_TAG = "CMC";
+    static final String APP_TAG = "CTP";
 
     static final String PREFERENCE_DATA_SOURCE = "data_source";
     static final String PREFERENCE_DISPLAY_FRAG = "display_frag";
