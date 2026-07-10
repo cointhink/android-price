7430cc3457e54258674118fd48d38dfcb3eed991
Don Park <donp@donp.org>
2026-07-10 16:06:13 +0700

    coinmarketcap fixup

--- app/src/main/java/com/cointhink/cmc/pricedata/CoinMarketCap.java
+++ app/src/main/java/com/cointhink/cmc/pricedata/CoinMarketCap.java
@@ -1,29 +1,30 @@
 package com.cointhink.cmc.pricedata;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.util.Log;
 
 import com.cointhink.cmc.Coin;
 import com.cointhink.cmc.Constants;
 import com.cointhink.cmc.Database;
 
-import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoinMarketCap implements Provider {
 
     public static String NAME = "coinmarketcap.com";
-    public static String COIN_URL = "https://api.coinmarketcap.com/v1/ticker/?limit=0";
+    public static String COIN_URL = "https://pro-api.coinmarketcap.com/public-api/v3/cryptocurrency/listings/latest?limit=200";
 
     @Override
     public List<Coin> parse(String json, Database db) {
         ArrayList<Coin> coins = null; // null signals err
         try {
             coins = new ArrayList<>();
-            JSONArray arr = new JSONArray(json);
+            JSONObject data = new JSONObject(json);
+            JSONArray arr = data.getJSONArray("data");
             Log.d(Constants.APP_TAG,
                     "coinmarketcap parsing " + arr.length() + " records");
             for (int i = 0; i < arr.length(); i++) {
@@ -35,7 +36,7 @@ public class CoinMarketCap implements Provider {
         } catch (JSONException e) {
             String eStr = e.toString();
             String eMsg = eStr.length() > 199 ? eStr.substring(0, 200)
-                    + eStr.substring(eStr.length() - 200) : eStr;
+                                                + eStr.substring(eStr.length() - 200) : eStr;
             Log.e(Constants.APP_TAG, "Error parsing f/e " + eMsg);
         }
         return coins;
@@ -46,12 +47,14 @@ public class CoinMarketCap implements Provider {
         Coin coin = new Coin();
         coin.name = o.getString("name");
         coin.symbol = o.getString("symbol");
-        coin.price = o.getString("price_usd");
-        coin.marketCap = o.getString("market_cap_usd");
-        coin.vol_24h = o.getString("24h_volume_usd");
-        coin.chg_1h = o.getString("percent_change_1h");
-        coin.chg_24h = o.getString("percent_change_24h");
-        coin.chg_7d = o.getString("percent_change_7d");
+        JSONArray quotes = o.getJSONArray("quote");
+        JSONObject quote = quotes.getJSONObject(0);
+        coin.price = quote.getString("price");
+        coin.marketCap = quote.optString("market_cap", "0");
+        coin.vol_24h = quote.optString("volume_24h", "0");
+        coin.chg_1h = quote.optString("percent_change_1h", "0");
+        coin.chg_24h = quote.optString("percent_change_24h", "0");
+        coin.chg_7d = quote.optString("percent_change_7d", "0");
 
         coin.img_url = imgUrl(coin.name);
         return coin;
@@ -69,7 +72,8 @@ public class CoinMarketCap implements Provider {
 
     static String imgUrl(String name) {
         //https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png
-        String url = "https://files.coinmarketcap.com/static/img/coins/32x32/";
+        //https://s2.coinmarketcap.com/static/img/coins/64x64/1.png
+        String url = "https://files.coinmarketcap.com/static/img/coins/64x64/";
         String urlName = name.toLowerCase().replaceAll(" ", "-");
         return url + urlName + ".png";
     }
